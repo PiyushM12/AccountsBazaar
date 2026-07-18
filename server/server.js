6b2e492739bf7384678dc3fc710833609d4c7ec8
PIYUSH MISHRA <piyushhmishraaa@gmail.com>
2026-07-18 10:57:22 +0530

    added vercel config

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 import express from "express"
 import "dotenv/config"
 import cors from "cors"
+import {clerkMiddleware} from '@clerk/express'
+import {serve} from "inngest/express"
+import { inngest, functions} from './inngest/index.js'
 
 const app = express()
 app.use(express.json());
 app.use(cors())
+app.use(clerkMiddleware())
 
 app.get("/",(req,res)=>res.send("Server IS live!"))
+
+app.use("/api/inngest",serve({client:inngest,functions}))
 
 const PORT = process.env.PORT || 3000;
 
